bc6e6c44b1b3a530d8e61f16ff8d178afa2e4a7a
rieg-ec <rieg.10199@gmail.com>
2022-09-27 20:17:00 -0300

    init

---
+++ Proyecto Base.srcs/sources_1/new/CPU.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+
+
+entity CPU is
+    Port (
+           clock : in STD_LOGIC;
+           clear : in STD_LOGIC;
+           ram_address : out STD_LOGIC_VECTOR (11 downto 0);
+           ram_datain : out STD_LOGIC_VECTOR (15 downto 0);
+           ram_dataout : in STD_LOGIC_VECTOR (15 downto 0);
+           ram_write : out STD_LOGIC;
+           rom_address : out STD_LOGIC_VECTOR (11 downto 0);
+           rom_dataout : in STD_LOGIC_VECTOR (35 downto 0);
+           dis : out STD_LOGIC_VECTOR (15 downto 0));
+end CPU;
+
+architecture Behavioral of CPU is
+
+begin
+
+end Behavioral;
+
